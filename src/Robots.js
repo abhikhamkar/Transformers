84f6076170439fdc159c6429d3989869004a7493
abhijit.khamkar <abhijit.khamkar@decos.com>
2021-09-13 21:19:56 +0530

    first commit

---
+++ src/Robots.js
@@ -0,0 +1,122 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Arcee',
+        username: 'Arcee',
+        email:'Arcee@Transformers.com'
+    },
+    {
+        id: 2,
+        name: 'Sunstreaker',
+        username: 'Sunstreaker',
+        email:'Sunstreaker@Transformers.com'
+    },
+    {
+        id: 3,
+        name: 'Wheeljack',
+        username: 'Wheeljack',
+        email:'Wheeljack@Transformers.com'
+    },
+    {
+        id: 4,
+        name: 'Optimus Prime',
+        username: 'Optimus Prime',
+        email:'OptimusPrime@Transformers.com'
+    },
+    {
+        id: 5,
+        name: 'Cliffjumper',
+        username: 'Cliffjumpe',
+        email:'Cliffjumpe@Transformers.com'
+    },
+    {
+        id: 6,
+        name: 'Bumblebee',
+        username: 'Bumblebee',
+        email:'Bumblebee@Transformers.com'
+    },
+    {
+        id: 7,
+        name: 'Ratchet',
+        username: 'Ratchet',
+        email:'Ratchet@Transformers.com'
+    },  
+    {
+        id: 8,
+        name: 'Jazz',
+        username: 'Jazz',
+        email:'Jazz@Transformers.com'
+    },
+    {
+        id: 9,
+        name: 'Trailbreaker',
+        username: 'Trailbreaker',
+        email:'Trailbreaker@Transformers.com'
+    },
+    {
+        id: 10,
+        name: 'Sideswipe',
+        username: 'Sideswipe',
+        email:'Sideswipe@Transformers.com'
+    },
+    {
+        id: 11,
+        name: 'Prowl',
+        username: 'Prowl',
+        email:'Prowl@Transformers.com'
+    },
+     {
+        id: 12,
+        name: 'Windcharger',
+        username: 'Windcharger',
+        email:'Windcharger@Transformers.com'
+    },
+     {
+        id: 13,
+        name: 'Jetfire',
+        username: 'Jetfire',
+        email:'Jetfire@Transformers.com'
+    },
+     {
+        id: 14,
+        name: 'Huffer',
+        username: 'Huffer',
+        email:'Huffer@Transformers.com'
+    },
+     {
+        id: 15,
+        name: 'Superion',
+        username: 'Superion',
+        email:'Superion@Transformers.com'
+    },
+    {
+        id: 16,
+        name: 'Bulkhead',
+        username: 'Bulkhead',
+        email:'Bulkhead@Transformers.com'
+    },
+     {
+        id: 17,
+        name: 'Brawn',
+        username: 'Brawn',
+        email:'Brawn@Transformers.com'
+    },
+     {
+        id: 18,
+        name: 'Perceptor',
+        username: 'Perceptor',
+        email:'Perceptor@Transformers.com'
+    },
+    {
+        id: 19,
+        name: 'Yoketron',
+        username: 'Yoketron',
+        email:'Yoketron@Transformers.com'
+    },
+     {
+        id: 20,
+        name: 'Bumper',
+        username: 'Bumper',
+        email:'Bumper@Transformers.com'
+    }
+];
---
+++ src/containers/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import ErrorBoundry from "../ErrorBoundry";
+import Scroll from "../components/Scroll.js";
+import { robots } from "../Robots";
+import "../containers/App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.setState({ robots: robots });
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    console.log("render");
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    return !robots.length ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">Transformer's</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
