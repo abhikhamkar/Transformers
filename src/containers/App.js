5b979923083ed77dd95c9f549cbe2ec2151020cb
abhijit.khamkar <abhijit.khamkar@decos.com>
2021-10-01 08:44:28 +0530

    Updated Title

--- src/containers/App.js
+++ src/containers/App.js
@@ -36,7 +36,7 @@ class App extends Component {
       <h1>Loading...</h1>
     ) : (
       <div className="tc">
-        <h1 className="f1">Transformer's</h1>
+        <h1 className="f1">Transformer Autobot's</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
           <ErrorBoundry>
